2c0c6000aa817a0c9c2acd25127075e4308667df
vernan2020 <65199148+vernan2020@users.noreply.github.com>
2026-09-07 09:49:10 -0600

    feat(financial-analysis): export peer rating row

--- src/aip/ui/modules/financial_analysis/viewmodels/__init__.py
+++ src/aip/ui/modules/financial_analysis/viewmodels/__init__.py
@@ -1,7 +1,13 @@
 from aip.ui.modules.financial_analysis.viewmodels.financial_analysis_view_model import (
     FinancialAnalysisViewModel,
+    PeerRatingRow,
     RatingDimensionRow,
     RatingIndicatorRow,
 )
 
-__all__ = ["FinancialAnalysisViewModel", "RatingDimensionRow", "RatingIndicatorRow"]
+__all__ = [
+    "FinancialAnalysisViewModel",
+    "PeerRatingRow",
+    "RatingDimensionRow",
+    "RatingIndicatorRow",
+]
