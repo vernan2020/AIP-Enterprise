b799de9ca5c0f512fcb972ac4ffbffa3c823cd8e
vernan2020 <65199148+vernan2020@users.noreply.github.com>
2026-09-09 17:21:47 -0600

    style: fix IRRBB presenter import order

--- src/aip/ui/modules/rate_risk/presenters/rate_risk_presenter.py
+++ src/aip/ui/modules/rate_risk/presenters/rate_risk_presenter.py
@@ -27,8 +27,8 @@ from aip.ui.modules.rate_risk.models.rate_risk_read_model import (
     RateRiskMappingRow,
     RateRiskMethodologyMetadata,
     RateRiskPositionQualityRow,
-    RateRiskReadModel,
     RateRiskReadinessSummary,
+    RateRiskReadModel,
     RateRiskScenarioRow,
     RateRiskValuationFlowRow,
 )
