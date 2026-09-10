cfd2a4d767fb20fac6427fb7791abb11fb4888c6
vernan2020 <65199148+vernan2020@users.noreply.github.com>
2026-09-10 12:24:06 -0600

    refactor(namespace): use relative SettlementDate import

--- src/aip/domain/portfolio/entities/position.py
+++ src/aip/domain/portfolio/entities/position.py
@@ -12,7 +12,6 @@ from src.aip.domain.portfolio.value_objects.isin import ISIN
 from src.aip.domain.portfolio.value_objects.market_value import MarketValue
 from src.aip.domain.portfolio.value_objects.nominal_value import NominalValue
 from src.aip.domain.portfolio.value_objects.quantity import Quantity
-from src.aip.domain.portfolio.value_objects.settlement_date import SettlementDate
 from src.aip.domain.portfolio.value_objects.yield_rate import YieldRate
 from src.aip.shared.money import Currency
 
@@ -20,6 +19,7 @@ from ..enums.position_status import PositionStatus
 from ..enums.valuation_source import ValuationSource
 from ..exceptions import InvalidPositionError
 from ..value_objects.position_id import PositionId
+from ..value_objects.settlement_date import SettlementDate
 
 
 @dataclass(slots=True)
