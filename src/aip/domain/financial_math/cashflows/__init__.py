ea60941310586bd4de781b17a13fc0c5750ea306
vernan2020 <65199148+vernan2020@users.noreply.github.com>
2026-09-13 14:19:16 -0600

    Slice 57: use relative imports in remaining financial_math barrels

--- src/aip/domain/financial_math/cashflows/__init__.py
+++ src/aip/domain/financial_math/cashflows/__init__.py
@@ -1,6 +1,6 @@
 """Cash flow domain objects."""
 
-from aip.domain.financial_math.cashflows.cashflow import CashFlow
-from aip.domain.financial_math.cashflows.cashflow_series import CashFlowSeries
+from .cashflow import CashFlow
+from .cashflow_series import CashFlowSeries
 
 __all__ = ["CashFlow", "CashFlowSeries"]
