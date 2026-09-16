dab9b4ab0cade7abf0fba3f71a09b380cc67f856
vernan2020 <65199148+vernan2020@users.noreply.github.com>
2026-09-16 11:08:05 -0600

    Slice 82: canonicalize analytics root barrel imports

--- src/aip/domain/analytics/__init__.py
+++ src/aip/domain/analytics/__init__.py
@@ -1,10 +1,10 @@
 """Analytics domain package."""
 
-from aip.domain.analytics.enums.normalization_method import NormalizationMethod
-from aip.domain.analytics.enums.outlier_method import OutlierMethod
-from aip.domain.analytics.enums.ranking_order import RankingOrder
-from aip.domain.analytics.enums.score_direction import ScoreDirection
-from aip.domain.analytics.exceptions import (
+from .enums.normalization_method import NormalizationMethod
+from .enums.outlier_method import OutlierMethod
+from .enums.ranking_order import RankingOrder
+from .enums.score_direction import ScoreDirection
+from .exceptions import (
     AnalyticsError,
     DuplicateRankItemError,
     ExplainabilityError,
@@ -15,26 +15,26 @@ from aip.domain.analytics.exceptions import (
     ScoringError,
     StatisticsError,
 )
-from aip.domain.analytics.explainability.explanation import Explanation
-from aip.domain.analytics.explainability.explanation_builder import ExplanationBuilder
-from aip.domain.analytics.explainability.explanation_factor import ExplanationFactor
-from aip.domain.analytics.models.analytics_context import AnalyticsContext
-from aip.domain.analytics.models.metric_observation import MetricObservation
-from aip.domain.analytics.normalization.min_max import MinMaxNormalizer
-from aip.domain.analytics.normalization.percentile_rank import PercentileRank
-from aip.domain.analytics.normalization.robust_scaler import RobustScaler
-from aip.domain.analytics.normalization.z_score import ZScoreNormalizer
-from aip.domain.analytics.ranking.rank_item import RankItem
-from aip.domain.analytics.ranking.ranking_engine import RankingEngine
-from aip.domain.analytics.ranking.ranking_result import RankingResult
-from aip.domain.analytics.ranking.tie_breaker import TieBreaker
-from aip.domain.analytics.scoring.composite_score import CompositeScore
-from aip.domain.analytics.scoring.score_band import ScoreBand
-from aip.domain.analytics.scoring.score_component import ScoreComponent
-from aip.domain.analytics.scoring.weighted_score import WeightedScore
-from aip.domain.analytics.statistics.descriptive_statistics import DescriptiveStatistics
-from aip.domain.analytics.statistics.outlier_detection import OutlierDetection
-from aip.domain.analytics.statistics.weighted_statistics import WeightedStatistics
+from .explainability.explanation import Explanation
+from .explainability.explanation_builder import ExplanationBuilder
+from .explainability.explanation_factor import ExplanationFactor
+from .models.analytics_context import AnalyticsContext
+from .models.metric_observation import MetricObservation
+from .normalization.min_max import MinMaxNormalizer
+from .normalization.percentile_rank import PercentileRank
+from .normalization.robust_scaler import RobustScaler
+from .normalization.z_score import ZScoreNormalizer
+from .ranking.rank_item import RankItem
+from .ranking.ranking_engine import RankingEngine
+from .ranking.ranking_result import RankingResult
+from .ranking.tie_breaker import TieBreaker
+from .scoring.composite_score import CompositeScore
+from .scoring.score_band import ScoreBand
+from .scoring.score_component import ScoreComponent
+from .scoring.weighted_score import WeightedScore
+from .statistics.descriptive_statistics import DescriptiveStatistics
+from .statistics.outlier_detection import OutlierDetection
+from .statistics.weighted_statistics import WeightedStatistics
 
 __all__ = [
     "AnalyticsContext",
