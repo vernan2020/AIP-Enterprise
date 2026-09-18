bdd3c5459dadc1f9fd0045f5ef41fa490f8f8609
vernan2020 <65199148+vernan2020@users.noreply.github.com>
2026-09-17 22:56:15 -0600

    Slice 145: use relative imports in relative value calculators

--- src/aip/domain/relative_value/calculators/z_spread.py
+++ src/aip/domain/relative_value/calculators/z_spread.py
@@ -3,7 +3,8 @@ from __future__ import annotations
 from decimal import Decimal
 
 from aip.domain.financial_math.curves.yield_curve import YieldCurve
-from aip.domain.relative_value.exceptions import SpreadCalculationError
+
+from ..exceptions import SpreadCalculationError
 
 
 class ZSpreadCalculator:
